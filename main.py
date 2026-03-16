92082e24a6d570d21aaeb6b7ae5507d0331b83f3
zugdurchfahrt <upolzuchiy@gmail.com>
2026-03-16 22:07:13 +0100

    16032206

--- main.py
+++ main.py
@@ -1265,7 +1265,7 @@ def main():
         configure_profile(driver, profile["language"], profile["languages"], country_data)
         
         # ----------------------- YOUR DESTINATION POINT, PLEASE MIND THE GAP -----------------------
-        driver.get("https://abrahamjuliot.github.io/creepjs/")
+        driver.get("https://abrahamjuliot.github.io/creepjs/tests/workers.html")
 
 
         # Keep main thread alive; otherwise daemon CDP threads die on process exit.
